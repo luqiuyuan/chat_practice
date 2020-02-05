e5eda28dc9f24a8cdde45835860415a4e2a908e6
luqiuyuan <sunlufinal@hotmail.com>
2020-02-05 18:08:32 -0500

    server-side codes

--- server/app/channels/ChatChannel.rb
+++ server/app/channels/ChatChannel.rb
@@ -1,19 +1,22 @@
 class ChatChannel < ApplicationCable::Channel
 
     def subscribed
-        puts "ping, subscribed"
-
-        stream_from "room_#{params[:room]}"
     end
 
     def unsubscribed
-        puts "pong, unsubscribed"
+        stop_all_streams
+    end
+
+    def follow
+        stream_from "room_#{params[:room]}"
+    end
+
+    def unfollow
+        stop_all_streams
     end
 
     def send_message(data)
-        puts "send message: " + data.to_s
-
-        ActionCable.server.broadcast "room_#{params[:room]}", content: data['content']
+        ActionCable.server.broadcast "room_#{params[:room]}", sender: data['sender'], content: data['content']
     end
 
 end
