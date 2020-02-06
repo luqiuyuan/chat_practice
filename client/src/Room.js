30f2062926fdc8c637ad92f95d1420f0a02a1e67
luqiuyuan <sunlufinal@hotmail.com>
2020-02-06 11:21:44 -0500

    add some components to page Room

--- client/src/Room.js
+++ client/src/Room.js
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import { Input } from 'antd';
+
 import './Room.css';
 
 export default function Room() {
   return (
     <div className="room_container">
+      <div className="room_messages">
+
+      </div>
+      <Input
+        className="room_input"
+        placeholder="Type your message" />
     </div>
   );
 }
